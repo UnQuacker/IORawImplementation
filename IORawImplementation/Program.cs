21b35e4287756d0fafe55f18d3ac823ad6a0da65
Talgat Sovetbekov <sarkytbayevquacker@gmail.com>
2022-06-23 16:08:18 +0600

    Add project files.

---
+++ IORawImplementation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace IORawImplementation
+{
+    class Program
+    {
+        [DllImport("IORawDll.dll")]
+        public static extern IntPtr Create();
+
+        [DllImport("IORawDll.dll")]
+        public static extern void TestWrite(IntPtr dll, string path, string data);
+
+        static void Main(string[] args)
+        {
+            IntPtr dll = Create();
+            string path = "C://Users//Alser//Desktop//test1.txt";
+            string data = "Raw data test";
+            TestWrite(dll, path, data);
+        }
+    }
+}
